f452c2589b55933055b327d2f0448b2fe82e079c
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-02-28 16:38:47 +0100

    adding pydantic

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Create the FastAPI app
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": " Hello, beautifull World 3.11"}
+    return {"message": " Hello, beautifull world 3.11"}
 
 
 

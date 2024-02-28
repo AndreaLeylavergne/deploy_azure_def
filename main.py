6abc9f76555a93eceb6b16ccec1d518cf177c41f
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-02-28 14:58:25 +0100

    back to helo World

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from contextlib import asynccontextmanager
-import tensorflow as tf
-from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing.text import Tokenizer
-import uvicorn
+from fastapi import FastAPI
 
 # Create the FastAPI app
 app = FastAPI()

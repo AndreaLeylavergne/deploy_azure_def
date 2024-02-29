df797b0256a5fa5f3c1bef19996fb564f7c96af9
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-02-29 08:42:15 +0100

    testing with tensorflow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from keras.models import load_model
+import tensorflow as tf
+
 # Create the FastAPI app
 app = FastAPI()
 

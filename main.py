69f4021c165d74f262c1738ad4a2de19ee7d21cb
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-03-01 06:43:44 +0100

    back to hello world requeriments

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from keras.models import load_model
+from fastapi import FastAPI
+
 
 # Create the FastAPI app
 app = FastAPI()

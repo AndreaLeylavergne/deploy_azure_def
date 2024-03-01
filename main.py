944b60866546a7f27bad320f745890e73e90e2b7
AndreaLeylavergne <119771926+AndreaLeylavergne@users.noreply.github.com>
2024-03-01 12:16:36 +0100

    adding prediction's endpoint

--- main.py
+++ main.py
@@ -44,6 +44,25 @@ with open('tokenizer.pickle', 'rb') as handle:
 # Padding des séquences pour avoir la même longueur
 max_length = 40
 
+def transformer_texte_en_sequence(textes, tokenizer, max_length):
+    sequences = [tokenizer.texts_to_sequences([text])[0] for text in textes]
+    sequences_pad = pad_sequences(sequences, maxlen=max_length)
+    return sequences_pad
+
+class InputData(BaseModel):
+    text: str
+
+@app.post("/predict")
+def predict_sentiment(input_data: InputData):
+    text = input_data.text
+    text_data = transformer_texte_en_sequence([text], tokenizer, max_length)
+    sentiment_pred = model.predict(text_data)
+    sentiment_score = sentiment_pred[0][0]
+    sentiment_label = "Positive" if sentiment_score >= 0.5 else "Negative"
+
+    return {"sentiment": sentiment_label, "score": float(sentiment_score)}
+
+
 @app.get("/")
 async def read_root():
     return {"message": " Hello, beautifull World 3.11"}
